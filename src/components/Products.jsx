9c28db7172d67195a36b0484e61d72e955847db5
slickcodes <hartpaulisimo@gmail.com>
2022-05-07 01:26:08 +0100

    add swiper package for and made the carousel with it

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,4 +1,5 @@
 import Product from "./Product";
+import React from 'react';
 
 /* eslint-disable jsx-a11y/anchor-is-valid */
 /* eslint-disable no-unused-vars */
@@ -7,12 +8,43 @@ import Product from "./Product";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowLeft, faArrowRight } from "@fortawesome/free-solid-svg-icons";
 
+import { Swiper, SwiperSlide , useSwiper} from "swiper/react";
+
+import "swiper/css";
+
 import "./../styles/css/products.css";
 const Products = function () {
   const recommendedProducts = [];
+  console.log(window.innerWidth)
+  const [ slidePerView , setSlidePerView ] = React.useState( window.innerWidth >= 950? 5 : 2 )
+  
+  function updateSlidePerView(){
+    console.log('working')
+    setSlidePerView( oldValue => {
+      const windowWidth = window.innerWidth
+
+      if(windowWidth >= 950)
+        return 5
+      else if (windowWidth >= 830)
+        return 4
+      else if (windowWidth >= 630) 
+      return 3
+      else 
+        return 2
+    } )
+  }
+  
+  window.onresize = updateSlidePerView
+
 
   for (let i = 0; i < 15; i++)
-    recommendedProducts.push(<Product key={i} title={"sdfa sdkfja sdf kasdfajsdfasdfasdfsfd"} />);
+    recommendedProducts.push(
+      <SwiperSlide  key={i} >
+        <Product title={"sdfa sdkfja sdf kasdfajsdfasdfasdfsfd"} />
+      </SwiperSlide>
+    );
+    let swiper = useSwiper()
+    console.log(swiper)
 
   return (
     <div className="products">
@@ -27,7 +59,16 @@ const Products = function () {
         </div>
         <section className="recommended">
           <h1 className="title">Recommended</h1>
-          <div className="recommended__product-container">{recommendedProducts}</div>
+          <div className="recommended__product-container">
+            <Swiper
+              slidesPerView={ slidePerView }
+              spaceBetween={8}
+              onSlideChange={() => console.log("slide just changed")}
+              onSwiper={(swiper) => console.log(swiper)}
+            >
+              {recommendedProducts}
+            </Swiper>
+          </div>
         </section>
       </div>
       {/* Products Section */}

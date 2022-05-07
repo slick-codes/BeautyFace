c67cbc0e6db2425635233d41414ee76fd31702fe
slickcodes <hartpaulisimo@gmail.com>
2022-05-07 10:23:08 +0100

    fixed desktop slide buttons and added basic functionalities

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,38 +1,42 @@
-import Product from "./Product";
-import React from 'react';
-
 /* eslint-disable jsx-a11y/anchor-is-valid */
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/jsx-no-comment-textnodes */
 
+// Importing Packages
+import React from 'react';
+import { Swiper, SwiperSlide , useSwiper} from "swiper/react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowLeft, faArrowRight } from "@fortawesome/free-solid-svg-icons";
 
-import { Swiper, SwiperSlide , useSwiper} from "swiper/react";
 
+// Imported Custome Components
+import Product from "./Product";
+// Imported Css
 import "swiper/css";
-
 import "./../styles/css/products.css";
+
+
 const Products = function () {
   const recommendedProducts = [];
-  console.log(window.innerWidth)
-  const [ slidePerView , setSlidePerView ] = React.useState( window.innerWidth >= 950? 5 : 2 )
+  const [ slidePerView , setSlidePerView ] = React.useState( 2 )
   
   function updateSlidePerView(){
-    console.log('working')
     setSlidePerView( oldValue => {
       const windowWidth = window.innerWidth
 
-      if(windowWidth >= 950)
-        return 5
-      else if (windowWidth >= 830)
-        return 4
-      else if (windowWidth >= 630) 
-      return 3
-      else 
-        return 2
+      if(windowWidth >= 950)  return 5
+      else if (windowWidth >= 830) return 4
+      else if (windowWidth >= 630) return 3
+      else return 2
+
     } )
   }
+
+  
+
+  React.useEffect( function(){
+    updateSlidePerView()
+  },[])
   
   window.onresize = updateSlidePerView
 
@@ -43,19 +47,30 @@ const Products = function () {
         <Product title={"sdfa sdkfja sdf kasdfajsdfasdfasdfsfd"} />
       </SwiperSlide>
     );
-    let swiper = useSwiper()
-    console.log(swiper)
 
+    const ref = React.useRef('swiper')
+    
+    function slideDir( dir ){
+      dir = dir.toLowerCase()
+      const element = ref.current
+      const elementChildWidth =  element.querySelector('.product').clientWidth
+      console.log(elementChildWidth)
+      const scrollLevel = element.offsetWidth / ( window.innerWidth >= 950? 2 : 1)  + elementChildWidth - 8
+      console.log(scrollLevel)
+      element.scrollBy( dir === 'prev'? - scrollLevel : scrollLevel , 0)
+    }
+    
+    
   return (
     <div className="products">
       <div className="products__recommended">
         <div className="button__container">
-        <div className="right__button">
+        <button className="right__button" onClick={ () => slideDir('prev') }>
           <FontAwesomeIcon icon={faArrowLeft} />
-        </div>
-        <div className="left__button">
+        </button>
+        <button className="left__button" onClick={ () => slideDir('next') }>
           <FontAwesomeIcon icon={faArrowRight} />
-        </div>
+        </button>
         </div>
         <section className="recommended">
           <h1 className="title">Recommended</h1>
@@ -63,8 +78,12 @@ const Products = function () {
             <Swiper
               slidesPerView={ slidePerView }
               spaceBetween={8}
-              onSlideChange={() => console.log("slide just changed")}
-              onSwiper={(swiper) => console.log(swiper)}
+              ref={ref}
+              scrollbar={{
+                draggable: true,
+              }}
+              // onSlideChange={() => console.log("slide just changed")}
+              // onSwiper={(swiper) => console.log(swiper)}
             >
               {recommendedProducts}
             </Swiper>

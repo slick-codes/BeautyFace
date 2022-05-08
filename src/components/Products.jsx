cff65e80b4fe07300a40ed7ec432fcee95318675
slickcodes <hartpaulisimo@gmail.com>
2022-05-08 09:34:15 +0100

    install react-multi-carousel for recommended section carousel and replaced the preveiws package

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -4,9 +4,15 @@
 
 // Importing Packages
 import React from "react";
-import { Swiper, SwiperSlide, useSwiper } from "swiper/react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowLeft, faArrowRight } from "@fortawesome/free-solid-svg-icons";
+// import { Swiper, SwiperSlide, useSwiper } from "swiper/react";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faArrowLeft, faArrowRight } from "@fortawesome/free-solid-svg-icons";
+
+import Carousel from "react-multi-carousel";
+import 'react-multi-carousel/lib/styles.css';
+
+
+import responsive from './../config/carousel-options'
 
 // Imported Custome Components
 import Product from "./Product";
@@ -38,92 +44,34 @@ const Products = function () {
 
   for (let i = 0; i < 14; i++)
     recommendedProducts.push(
-      <SwiperSlide key={i}>
-        <Product title={"sdfa sdkfja sdf kasdfajsdfasdfasdfsfd"} />
-      </SwiperSlide>
+        <Product key={i} title={"sdfa sdkfja sdf kasdfajsdfasdfasdfsfd"} />
     );
 
-  const ref = React.useRef("swiper");
-
-  function slideDir(dir) {
-    dir = dir.toLowerCase();
-    const element = ref.current;
-    const childElement = element.querySelector(".swiper-wrapper");
-        let childElementScrollWidth = childElement.scrollWidth
-
-    const parentElementWidth = element.offsetWidth + spaceBetween;
-
-    const transformValue = Number(
-      // extract the transform value from string
-      childElement.style.transform.split("(")[1].split(",")[0].split("px")[0]
-    );
-    let size = 0;
-
-    const products = element.querySelectorAll(".swiper-slide");
-
-    products.forEach((node) => {
-      size += node.clientWidth + spaceBetween + products.length;
-      
-    });
-
-    // TODO export function to external file
-    function isInViewport(element) {
-      const rect = element.getBoundingClientRect();
-      return rect.left;
-    }
-
-    const lastProduct = element.querySelector(".swiper-slide:last-child");
-    const lastProductInViewPort = isInViewport(lastProduct);
-
-    console.log(lastProductInViewPort, parentElementWidth);
-
-    const transitionValue = () => {
-      if (dir !== "prev") {
-        // console.log(transformValue - parentElementWidth, -size);
-
-        if (lastProductInViewPort <= parentElementWidth) {
-          console.log('got here');
-          return - childElementScrollWidth + parentElementWidth - 5
-        } else return transformValue - parentElementWidth;
-      } else {
-        if (transformValue + parentElementWidth > 0) {
-          return 0;
-        } else {
-          return transformValue + parentElementWidth;
-        }
-      }
-    };
-
-    childElement.style.transition = ".3s";
-    childElement.style.transform = `translate3D(${transitionValue()}px, 0px, 0px)`;
-  }
 
   return (
     <div className="products">
       <div className="products__recommended">
-        <div className="button__container">
-          <button className="right__button" onClick={() => slideDir("prev")}>
+        {/* <div className="button__container">
+          <button className="right__button">
             <FontAwesomeIcon icon={faArrowLeft} />
           </button>
-          <button className="left__button" onClick={() => slideDir("next")}>
+          <button className="left__button" >
             <FontAwesomeIcon icon={faArrowRight} />
           </button>
-        </div>
+        </div> */}
         <section className="recommended">
           <h1 className="title">Recommended</h1>
           <div className="recommended__product-container">
-            <Swiper
-              slidesPerView={slidePerView}
-              spaceBetween={spaceBetween}
-              ref={ref}
-              scrollbar={{
-                draggable: true,
-              }}
-              // onSlideChange={() => console.log("slide just changed")}
-              // onSwiper={(swiper) => console.log(swiper)}
+            <Carousel
+	    responsive={responsive}
+	    autoPlay={ false }
+	    infinite={ false }
+	//     autoPlaySpeed={ 3000}
+	    transitionDuration={ 0 }
+	    customTransition="all .3s"
             >
               {recommendedProducts}
-            </Swiper>
+            </Carousel>
           </div>
         </section>
       </div>
---
+++ src/config/carousel-options.js
@@ -0,0 +1,26 @@
+
+
+
+const  responsive = {
+    superLargeDesktop: {
+        breakpoint: { max: 4000, min: 950},
+        partialVisibilityGutter: 40,
+        items: 5
+    },
+    desktop:{
+        breakpoint: { max: 949, min: 830 },
+        items: 4
+    },
+    tablet: {
+        breakpoint: { max: 829, min: 629},
+        items: 3
+    },
+    mobile: {
+        breakpoint: { max: 629, min: 0},
+        items: 2
+    }
+}
+
+
+
+export default responsive 

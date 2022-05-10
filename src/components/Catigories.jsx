240c39a246f7139b1d2653a012b270c6c9795b96
slickcodes <hartpaulisimo@gmail.com>
2022-05-10 08:32:12 +0100

    add some stylings to the catigories section

--- src/components/Catigories.jsx
+++ src/components/Catigories.jsx
@@ -1,13 +1,91 @@
+// Packages
+// import Carousel from 'react-multi-carousel';
 
-
+// CSS
 import './../styles/css/catigories.css'
 
 function Catigories(){
 
 
     return (
-        <div className="catigories__container">
-            
+        <div className="catigories__cover">
+            <div className="catigories__nav">
+            <section className="title">Popular Catigories</section>
+            </div>
+            <div className="catigories__content">
+                <div className="catigory__card">
+                    <div className="catigories__image"></div>
+                    <div className="catigory__text">
+                        Shirt
+                    </div>
+                </div>
+                <div className="catigory__card">
+                    <div className="catigories__image"></div>
+                    <div className="catigory__text">
+                        Shoes
+                    </div>
+                </div>
+                <div className="catigory__card">
+                    <div className="catigories__image"></div>
+                    <div className="catigory__text">
+                        Jwueries
+                    </div>
+                </div>
+                <div className="catigory__card">
+                    <div className="catigories__image"></div>
+                    <div className="catigory__text">
+                        Jwueries
+                    </div>
+                </div>
+                <div className="catigory__card">
+                    <div className="catigories__image"></div>
+                    <div className="catigory__text">
+                        Jwueries
+                    </div>
+                </div>
+                <div className="catigory__card">
+                    <div className="catigories__image"></div>
+                    <div className="catigory__text">
+                        Jwueries
+                    </div>
+                </div>
+                <div className="catigory__card">
+                    <div className="catigories__image"></div>
+                    <div className="catigory__text">
+                        Jwueries
+                    </div>
+                </div>
+                <div className="catigory__card">
+                    <div className="catigories__image"></div>
+                    <div className="catigory__text">
+                        Jwueries
+                    </div>
+                </div>
+                <div className="catigory__card">
+                    <div className="catigories__image"></div>
+                    <div className="catigory__text">
+                        Jwueries
+                    </div>
+                </div>
+                <div className="catigory__card">
+                    <div className="catigories__image"></div>
+                    <div className="catigory__text">
+                        Jwueries
+                    </div>
+                </div>
+                <div className="catigory__card">
+                    <div className="catigories__image"></div>
+                    <div className="catigory__text">
+                        Jwueries
+                    </div>
+                </div>
+                <div className="catigory__card">
+                    <div className="catigories__image"></div>
+                    <div className="catigory__text">
+                        Jwueries
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
